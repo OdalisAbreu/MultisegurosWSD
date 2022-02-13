0e5572cb65a5b417e733982f4b55a31d680bc30f
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-13 18:39:36 -0400

    V 1.1.10

--- controller/Records.php
+++ controller/Records.php
@@ -1,9 +1,10 @@
 <?php
-
     class records{
         
         function newRecord($userId, $type, $references){
+
            $record =  mysql_query("INSERT INTO records (user_id, type, references, date) VALUES ('".$userId."', '".$type."', '".$references."', '".date('Y/m/d H:i:s')."')");
-            exit('14/'.$record.'/00');
+           
+           exit('14/'.$record.'/00');
         }
     }
