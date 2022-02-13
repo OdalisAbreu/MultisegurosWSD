2b388cb70bbe7aba5728f67940ea76b9f72dac2b
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-13 19:26:19 -0400

    V 1.1.20

--- controller/Records.php
+++ controller/Records.php
@@ -3,7 +3,7 @@
         
         function newRecord($userId, $type, $references){
            $record =  mysql_query("INSERT INTO records (user_id, type, references, date) VALUES 
-                                ('" . $userId . "','" . $type . "','" . $references . "','" . date("Y-m-d H:i:s") . "')");
+                                ('" . $userId . "','" . $type . "','" . $references . "','" .date('Y-m-d H:i:s'). "')");
     
            if (!$record) {
             exit('144/'.die('Consulta no válida: ' . mysql_error()).' NUM/00');
