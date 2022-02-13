519ac023c75045191e1830c26f0a40a0499fe57e
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-13 19:17:21 -0400

    V 1.1.16

--- controller/Records.php
+++ controller/Records.php
@@ -2,9 +2,17 @@
     class records{
         
         function newRecord($userId, $type, $references){
-            $date = date('Y/m/d H:i:s');
-           $record =  mysql_query("INSERT INTO records (user_id, type, references, date) VALUES ('".$userId."', '".$type."', '".$references."', '".$date."')");
-           
+            $date = 
+           $record =  mysql_query(
+            "INSERT INTO records (user_id, type, references, date) 
+            VALUES 
+            ('" . $userId . "',
+            '" . $type . "',
+            '" . $references . "',
+            '" . date("Y-m-d H:i:s") . "'
+            )"
+        );
+    
            if (!$record) {
             exit('144/'.die('Consulta no válida: ' . mysql_error()).' NUM/00');
             }
