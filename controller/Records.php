e1fc1786b1bc7ab1d44789905555d969b99a2834
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-13 23:15:27 -0400

    V 1.1.29

--- controller/Records.php
+++ controller/Records.php
@@ -2,9 +2,9 @@
     class records{
         
         function newRecord($userId, $type, $references){
-           $record =  mysql_query("INSERT INTO `multiseg_2`.`records` (`id_user`, `type`, `references`, `date`) VALUES ('4', 'Venta Poliza', ' 50757', '".date("Y-m-d H:i:s")."')");
+           $record =  mysql_query("INSERT INTO `multiseg_2`.`records` (`id_user`, `type`, `references`, `date`) VALUES ('".$userId."', '".$type."', '".$references."', '".date("Y-m-d H:i:s")."')");
            if (!$record) {
-            exit('144/'.die('Consulta no válida: ' . mysql_error()).'/00');
+            exit('144/'.die('Consulta inválida: ' . mysql_error()).'/00');
             }
         }
     }
