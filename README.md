c64ec910ff3b391106971230d20b6edac1d61385
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-21 00:43:30 -0400

    V 1.1.48

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-V 1.1.47 //Log al momento de ingresar una marca invalida
+V 1.1.48 //Log al momento de ingresar una marca invalida
 
 1. Agregar Lin. 65-66-72 a Get_Total Poliza to Prod
--- Seguros/GET_Poliza_Total.php
+++ Seguros/GET_Poliza_Total.php
@@ -60,17 +60,19 @@ if ($_REQUEST['idApi'] == '2wessd@d3e') {
 
 error_log(json_encode($_POST));
 if ($_POST) {
-	$hoy = date("Y-m-d");
+	$hoy = date("Ymd");
 	$Choy = str_replace("-", "", $_POST['fecha_inicio']);
 	$fechaActual = new DateTime($hoy);
 	$fechaEnviada = new DateTime($Choy);
-	exit("30/La Fecha de inicio de la poliza debe ser igual o superior a: ".$fechaActual." - ".$fechaEnviada."/00 ");
+	var_dump($fechaActual);
+	var_dump($fechaEnviada);
+	exit("30/La Fecha de inicio de la poliza debe ser igual o superior a: ".$fechaActual." y ".$fechaEnviada."/00 ");
 	if ($fechaEnviada < $fechaActual) {
 
 		$_POST['fecha_inicio'] = date("Y-m-d");
 		Auditoria($_POST['usuario'], $_POST['clave'], '', "Error en fecha, fecha no identificada, usted envio REQUEST:${$_REQUEST['fecha_inicio']}, HOY:$hoy, CHOY:$Choy, PV: ${$_POST['xID']} ", 'venta_error', '17', '', '');
 		exit("30/La Fecha de inicio de la poliza debe ser igual o superior a: ".$hoy."/00 ");
-	} elseif ($fechaEnviada > $fechaActual) {
+	} elseif ($Choy > $hoy) {
 
 		$_POST['fecha_inicio'] = $_POST['fecha_inicio'];
 	}
