1839f0e5dc273ac849e32f2a457e1ad905817301
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-21 23:37:29 -0400

    V 1.1.60

--- Seguros/GET_Poliza_Total.php
+++ Seguros/GET_Poliza_Total.php
@@ -61,14 +61,14 @@ if ($_REQUEST['idApi'] == '2wessd@d3e') {
 error_log(json_encode($_POST));
 if ($_POST) {
 	$hoy = date("Y-m-d");
-	$Choy = $_POST['fecha_inicio'];
+	$Choy =  $_POST['fecha_inicio'];
 	
 	$fechaActual = date_create($hoy);
 	$fechaPoliza = date_create($Choy);
-	//$cont = date_diff($fechaPoliza, $fechaActual);
+	$cont = (array) date_diff($fechaPoliza, $fechaActual);
 
-	//exit("22/".$cont->format('%a')."/00 ");
-	exit("22/".$fechaActual." - ".$fechaPoliza."/00 ");
+	exit("22/".$cont['d']."/00 ");
+
 
 	if ($Choy < $hoy) {
 
